096abc757a4e17f953993273e1579505426d2c61
f9lmw <n0yuza1@gmail.com>
2021-04-18 09:13:59 +0300

    persik music log form

---
+++ PersBuyMus.js
@@ -0,0 +1,4 @@
+var  button= document.querySelector('.button');
+button.onclick = function() {
+    window.location.href = "Bougth.html";
+}
---
+++ PersMusReg.js
@@ -0,0 +1,4 @@
+var button = document.querySelector('.btn');
+button.onclick = function() {
+    window.location.href = "PersBuyMus.html";
+}
